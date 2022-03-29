f5b691072ed6137e89eeb29dd054f57919ddad73
moad6127 <moad6127@gmail.com>
2022-03-29 16:27:34 +0900

    fix : 클래스 맴버함수의 암시적 형태
    this 암시적

--- class/main.cpp
+++ class/main.cpp
@@ -1,52 +1,37 @@
+
 #include<iostream>
 
-
-class  Point2D
+struct Dog
 {
-	int mX;
-	int mY;
+	int age;
+};
+
+int GetDogAge(Dog& dog)
+{
+	return dog.age;
+}
+//-------------------------------
+class Cat
+{
+	int mAge;
 
 public:
-	void SetX(int x);
-	void SetY(int y);
-	void Set(int x, int y);
-	int GetX();
-	int GetY();
-	void Print();	
+	int mGetAge()
+	{
+		//Implicity Object
+		// this :객체의 포인터
+
+		//return mAge;
+		//Explicity 
+		return this->mAge;
+	}
 };
 
 int main()
 {
-	Point2D pt1;
+	Dog Jhon;
+	std::cout << GetDogAge(Jhon) << std::endl;
 
-	pt1.Set(1, 1);
+	Cat nacho;
+	std::cout << nacho.mGetAge() << std::endl;
 }
-
-void Point2D::SetX(int x)
-{
-	mX = x;
-}
-
-void Point2D::SetY(int y)
-{
-	mY = y;
-}
-
-void Point2D::Set(int x, int y)
-{
-	mX = x;
-	mY = y;
-}
-
-int Point2D::GetX()
-{
-	std::cout << mX << std::endl;
-	return mX;
-}
-
-int Point2D::GetY()
-{
-	std::cout << mY << std::endl;
-	return mY;
-}
-
