c3996ac6f85dc093a6f94d87cafdf6e65da13739
moad6127 <moad6127@gmail.com>
2022-03-29 15:53:41 +0900

    feat : 수업자료
    멤버변수에 한번에 접근못하게 하기
    함수를 통해 접근하게하기

--- class/main.cpp
+++ class/main.cpp
@@ -1,6 +1,52 @@
 #include<iostream>
 
+
+class  Point2D
+{
+	int mX;
+	int mY;
+
+public:
+	void SetX(int x);
+	void SetY(int y);
+	void Set(int x, int y);
+	int GetX();
+	int GetY();
+	void Print();	
+};
+
 int main()
 {
+	Point2D pt1;
 
+	pt1.Set(1, 1);
 }
+
+void Point2D::SetX(int x)
+{
+	mX = x;
+}
+
+void Point2D::SetY(int y)
+{
+	mY = y;
+}
+
+void Point2D::Set(int x, int y)
+{
+	mX = x;
+	mY = y;
+}
+
+int Point2D::GetX()
+{
+	std::cout << mX << std::endl;
+	return mX;
+}
+
+int Point2D::GetY()
+{
+	std::cout << mY << std::endl;
+	return mY;
+}
+
