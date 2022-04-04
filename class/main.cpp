57ae2a4066d1abffeca9f3783b87beb86afad7d8
moad6127 <moad6127@gmail.com>
2022-04-04 15:16:23 +0900

    Feat : 상수 클래스
    상수 클래스

--- class/main.cpp
+++ class/main.cpp
@@ -1,25 +1,41 @@
 #include<iostream>
 
-	class MyArray
-	{
-	private:
-		int mLength;
-		int* mArray;
+class MyClass
+{
+private:
+	int mValue1;
+	int mValue2;
 
-	public:
-		MyArray(int length) : mLength{ length }
-		{
-			mArray = new int[length] {};
-		}
-		~MyArray()
-		{
-			delete[] mArray;
-			mArray = nullptr;
-		}
-	
-	};
-
-	int main()
+public:
+	MyClass() : MyClass(1,1)
 	{
-		MyArray array{ 20 };
+
 	}
+	MyClass(int v1, int v2) : mValue1{ v1 }, mValue2{ v2 }
+	{
+
+	}
+	void SetValue1(int x)
+	{
+		mValue1 = x;
+	}
+	void Print() const
+	{
+		std::cout << mValue1 << ", " << mValue2 << std::endl;
+	}
+};
+
+
+int main()
+{
+	const MyClass c1;
+	const MyClass c2{ 2,2 };
+	const MyClass c3{ 3,3 };
+	MyClass c4{ 4,4 };
+	c4.SetValue1(10);
+
+	c1.Print();
+	c2.Print();
+	c3.Print();
+	c4.Print();
+}
