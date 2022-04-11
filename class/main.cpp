9b492e2721762d0470ef4e2dca723d0fa142a6d4
moad6127 <moad6127@gmail.com>
2022-04-11 16:54:02 +0900

    Feat : Polymorphism
    다형성

--- class/main.cpp
+++ class/main.cpp
@@ -2,10 +2,11 @@
 #include"Dog.h"
 int main()
 {
-	Dog happy{1,1,Dog::BEAGEL}; // == Dog happy = Dog();
+	Dog Happy;
+	Animal* pDog = &Happy; // happy는 dog이지만 animal이기도 하다.
+	Animal* pAnim = new Dog();
 
-	happy.Sound();
-	happy.Roll();
-	happy.SetAge(3);
+	pAnim->Sound(); //이때는 Animal의 Sound가 호출된다.
 	
+	delete pAnim;
 }
