e26574444faf4b67401e4fb5ab68e3e294c45244
moad6127 <moad6127@gmail.com>
2022-04-13 15:37:03 +0900

    Feat : typecast
    형변환
    static_cast
    dynamic_cast

--- class/main.cpp
+++ class/main.cpp
@@ -3,10 +3,30 @@
 
 int main()
 {
+	float x = 10.5f;
+	//C++ style
+	int y = static_cast<int>(x);
+	//C style
+	int z = int(x);
+
+	std::cout << y << std::endl;
+	std::cout << z << std::endl;
+
+
 
 	Character* pHero = new Warrior;
 	pHero->Attack();
-	
+	//((Warrior*)pHero)->DoubleSlash(); //형변환을 통해서 전용스킬 사용
+	Warrior* pWarrior = dynamic_cast<Warrior*>(pHero);
+	if (pWarrior == nullptr) //형변환 실패시
+	{
+		std::cout << "ERROR" << std::endl;
+	}
+	else
+	{
+		pWarrior->DoubleSlash();
+	}
+
 	delete pHero;
 
 
