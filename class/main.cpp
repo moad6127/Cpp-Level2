b34bc6b2acf204521a378119b9bf9d80814c4e8e
moad6127 <moad6127@gmail.com>
2022-04-14 15:23:37 +0900

    Feat : namespace
    이름 공간
    using namespace

--- class/main.cpp
+++ class/main.cpp
@@ -1,57 +1,31 @@
 #include <iostream>
-#include"Warrior.h"
 
-class MyFunctions
+using namespace std;
+using namespace Character;
+
+
+namespace Character
 {
-public:
-	//f(x) =x
-	int NomalFunction(int x)
+	class Warrior
 	{
-		return x;
-	}
-	//f(x) = x^2
-	int SquareFunction(int x)
+	public:
+		void DoulbeSlash();
+	};
+	//플레이어의 정보를 출력
+	void Print()
 	{
-		return x * x;
+
 	}
-};
-using FunctionPtr = int(MyFunctions::*)(int);
-int Sigma(MyFunctions& instance, FunctionPtr f, int m, int n)
-{
-	int sum{};
-	for (int i = m; i <= n; i++)
-	{
-		sum +=(instance.*f)(i);
-	}
-	return sum;
 }
 
-
+void Print()
+{
+	std::cout << "Global!" << std::endl;
+}
 int main()
 {
-	MyFunctions function;
-
-	//함수 포인터
-	std::cout << Sigma(function,&MyFunctions::NomalFunction, 1, 100) << std::endl;
-	std::cout << Sigma(function,&MyFunctions::SquareFunction, 1, 10) << std::endl;
-
-
-
-	//Character* pHero = new Warrior;
-	//pHero->Attack();
-	////((Warrior*)pHero)->DoubleSlash(); //형변환을 통해서 전용스킬 사용
-	//Warrior* pWarrior = dynamic_cast<Warrior*>(pHero);
-	//if (pWarrior == nullptr) //형변환 실패시
-	//{
-	//	std::cout << "ERROR" << std::endl;
-	//}
-	//else
-	//{
-	//	pWarrior->DoubleSlash();
-	//}
-
-	//pHero->Dead();
-	//delete pHero;
-
-
+	Warrior player;
+	player.DoulbeSlash();
+	Character::Print();
+	::Print();
 }
