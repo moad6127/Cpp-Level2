87c0d41e1774c90c64bc8a13fedfa4f156d83614
moad6127 <moad6127@gmail.com>
2022-04-14 16:13:04 +0900

    Feat : template
    탬플릿으로 함수만들

--- class/main.cpp
+++ class/main.cpp
@@ -1,31 +1,42 @@
 #include <iostream>
 
 using namespace std;
-using namespace Character;
-
-
-namespace Character
+//T Function(T arg1, T arg2)
+//{
+//	T result = arg1 + arg2;
+//	return result;
+//}
+//int main()
+//{
+//	int x{ 1 }, y{ 2 };
+//	cout << Function<int>(x, y) << endl;
+//	cout << Function<float>(1.9f, 3.2f) << endl;
+//}
+template <typename T>
+void Sort(T input[], int size)
 {
-	class Warrior
+	for (int i = 0; i< size - 1; i++)
 	{
-	public:
-		void DoulbeSlash();
-	};
-	//플레이어의 정보를 출력
-	void Print()
-	{
+		for (int j = i + 1; j< size; j++)
+		{
 
+			if (input[i] > input[j])
+			{
+				T temp = input[i];
+				input[i] = input[j];
+				input[j] = temp;
+			}
+		}
 	}
-}
-
-void Print()
-{
-	std::cout << "Global!" << std::endl;
 }
 int main()
 {
-	Warrior player;
-	player.DoulbeSlash();
-	Character::Print();
-	::Print();
+	int array[]{ 5,3,2,4,1 };
+
+	Sort<int>(array, 5);
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << array[i] << endl;
+	}
 }
