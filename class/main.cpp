ff8ecf4267f9b3d6c7a8f154bd2b9c16c4230bc2
moad6127 <moad6127@gmail.com>
2022-04-15 16:47:51 +0900

    Feat :  C++ STL vector
    벡터 사용하기

--- class/main.cpp
+++ class/main.cpp
@@ -1,62 +1,36 @@
 #include <iostream>
-#include <string>
-void Print(const std::string& str)
-{
-	std::cout << str.size() << " : " << str << std::endl;
-}
+#include<vector>
+
 int main()
 {
-
-	std::string empty;
-	Print(empty);
-	std::string sized(10, '1');
-	Print(sized);
-	std::string s1("test");
-	std::cout << s1[0] << s1[1] << s1[2] << s1[3] << std::endl;
-
-
-	std::string s2(s1);
-	Print(s2);
-
-	std::string s3 = s1 + s2;
-	Print(s3);
-	empty = s3;
-	Print(empty);
-
-	empty = empty;
-	Print(empty);
-
-	int number = 123;
-	std::string numString;
-	numString = std::to_string(number);		//123 ->"123"
-	number = std::stoi(numString);			//"123" ->123
-
-	//std::string s1{ "abcd" };
-	//std::string s2{ "1234" };
-
-	//std::string s3{ s1 + s2 };
-	//s1 = s2;
-	//s2 = std::string("hello") + std::string("world");
-	//s1 += s2;
-
-	//s2[0] = 'H';
-	//if (s1 == s2) //s1 != s2, s1>s2 가능하다
-	//{
-
-	//}
-	//s1.c_str(); //C스타일의 문자열을 만듬(캐릭터포인터)
-
-	//////container 특성
-	//s1.size();
-	//s1.insert();
-	//s1.append();
-	//s1.erase();
-	//s1.find(2);
-
-	////iterator
-	//s1.begin();
-	//s1.end();
+	//vector :  연속된 n개의 값<집합>
+	//std::vector<int> v1{ 2,3,1,5,6 };
+	//v1.push_back(4);
 	//
-	//std::cout << s1;
-	//std::cin >> s1;
+	////range-base loop [0 : siez)
+	//for (int elemmnt : v1)
+	//{
+	//	std::cout << elemmnt << std::endl;
+	//}
+
+	//숫자들을 입력 받고 총합계와 평균을 구해보기
+	//예) 1 2 3 6 4 8 0 4 10 d
+	//숫자를 공백으로 구분하여 입력
+	//입력 종료는 수자가 아닌 문자를 입력
+	std::vector<int> vScore;
+	int value;
+	std::cout << "숫자를 입력하세요 : " << std::endl;
+	while (std::cin >> value)
+	{
+		vScore.push_back(value);
+	}
+	int Sum{};
+	float average{};
+	for (int e : vScore)
+	{
+		Sum += e;
+	}
+	average = static_cast<float>(Sum) / vScore.size();
+	std::cout <<"합계 : " << Sum << std::endl;
+	std::cout <<"평균 : " << average << std::endl;
 }
