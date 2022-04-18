5874d9bdffbf4be9606e18b4ac8d2b0135ce3ab7
moad6127 <moad6127@gmail.com>
2022-04-18 15:41:20 +0900

    Feat : range based loop

--- class/main.cpp
+++ class/main.cpp
@@ -3,30 +3,33 @@
 
 int main()
 {
+	std::vector<int> v{ 1,2,3,4,5, };
 
-	std::vector<int> v{ 1,2,3 };
-
-	std::vector<int>::iterator itr;// 포인터이다.
-
-	for (auto e : v)
+	//range besed for
+	//대부분의 경우
+	for (int& e : v) // 참조형으로 만들면 v의 원소를 바꿀수있게 된다.
 	{
-		std::cout << e << '\n';
+		e = 1;
+	}
+	//index for loop
+	//인덱스가 필요한 경우
+	for (int i = 0; i < v.size(); ++i)
+	{
+		std::cout << v[i] << std::endl;
 	}
 
-	//iterator while
-	itr = v.begin();
-	// while문 으로 하기
-	while (itr != v.end()) 
+	//STL과 관련된 모든 작업
+
+	std::vector<int>::iterator itr = v.begin();
+	while (itr != v.end())
 	{
+		*itr = 1;
 		std::cout << *itr << std::endl;
 		itr++;
 	}
-	//for문으로 만들기
-	//타입을 추측할수 있으면 auto를 사용할수 있다.
 
-	for (auto itr = v.begin(); itr != v.end(); itr++)
+	for (const auto& e : v) //이렇게 하면 v의 값을 변경할수 없다.
 	{
-		std::cout << *itr << std::endl;
+		std::cout << e << std::endl;
 	}
-
 }
