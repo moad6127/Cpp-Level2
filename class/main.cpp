a66bc62e2fb244b97af09bf6bd3b808dd282c444
moad6127 <moad6127@gmail.com>
2022-04-18 15:14:05 +0900

    Feat  : auto
    auto 타입 추측

--- class/main.cpp
+++ class/main.cpp
@@ -8,6 +8,12 @@ int main()
 
 	std::vector<int>::iterator itr;// 포인터이다.
 
+	for (auto e : v)
+	{
+		std::cout << e << '\n';
+	}
+
+	//iterator while
 	itr = v.begin();
 	// while문 으로 하기
 	while (itr != v.end()) 
@@ -16,8 +22,11 @@ int main()
 		itr++;
 	}
 	//for문으로 만들기
-	for (std::vector<int>::iterator itr = v.begin(); itr != v.end(); itr++)
+	//타입을 추측할수 있으면 auto를 사용할수 있다.
+
+	for (auto itr = v.begin(); itr != v.end(); itr++)
 	{
 		std::cout << *itr << std::endl;
 	}
+
 }
