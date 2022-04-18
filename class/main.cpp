5b64ef07f81bef921a7695bf3752b1722e865e39
moad6127 <moad6127@gmail.com>
2022-04-18 16:47:31 +0900

    Feat : Lamda Expression
    람다식

--- class/main.cpp
+++ class/main.cpp
@@ -2,33 +2,32 @@
 #include<vector>
 #include<algorithm>
 
-int compare(const void* left, const void* right)
-{
-	int arg1 = *static_cast<const int*> (left);
-	int arg2 = *static_cast<const int*>(right);
-	//if (arg1 > arg2)
-	//{
-	//	return -1;
-	//}
-	//else if (arg1 < arg2)
-	//{
-	//	return 1;
-	//}
-	//else
-	//{
-	//	return 0;
-	//}
-	return arg1- arg2;
-}
+//int compare(const void* left, const void* right)
+//{
+//	int arg1 = *static_cast<const int*> (left);
+//	int arg2 = *static_cast<const int*>(right);
+//
+//	return arg1- arg2;
+//}
+
+
 int main()
 {
 	std::vector<int> v{5,4,3,2,1 };
 
 	//C-style
-	//std::qsort(&v[0],v.size(),sizeof(int),compare);
+	std::qsort(&v[0],v.size(),sizeof(int),
+				[](const void* left , const void* right) ->int
+				{
+				int arg1 = *static_cast<const int*> (left);
+				int arg2 = *static_cast<const int*>(right);
+
+				return arg1 - arg2; 
+				}
+	);
 
 	//C++ style
-	std::sort(v.begin(), v.end());
+	//std::sort(v.begin(), v.end());
 
 
 	for (const auto& e : v)
