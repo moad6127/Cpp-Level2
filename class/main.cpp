b32b539f669815eac8b6572f7b04d44a8c03c2c6
moad6127 <moad6127@gmail.com>
2022-04-18 16:41:06 +0900

    Feat : algorithm
    알고리즘 알아보기
    (qsort, sort)

--- class/main.cpp
+++ class/main.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 #include<vector>
+#include<algorithm>
 
+int compare(const void* left, const void* right)
+{
+	int arg1 = *static_cast<const int*> (left);
+	int arg2 = *static_cast<const int*>(right);
+	//if (arg1 > arg2)
+	//{
+	//	return -1;
+	//}
+	//else if (arg1 < arg2)
+	//{
+	//	return 1;
+	//}
+	//else
+	//{
+	//	return 0;
+	//}
+	return arg1- arg2;
+}
 int main()
 {
-	std::vector<int> v{ 1,2,3,4,5, };
+	std::vector<int> v{5,4,3,2,1 };
 
-	//range besed for
-	//대부분의 경우
-	for (int& e : v) // 참조형으로 만들면 v의 원소를 바꿀수있게 된다.
-	{
-		e = 1;
-	}
-	//index for loop
-	//인덱스가 필요한 경우
-	for (int i = 0; i < v.size(); ++i)
-	{
-		std::cout << v[i] << std::endl;
-	}
+	//C-style
+	//std::qsort(&v[0],v.size(),sizeof(int),compare);
 
-	//STL과 관련된 모든 작업
+	//C++ style
+	std::sort(v.begin(), v.end());
 
-	std::vector<int>::iterator itr = v.begin();
-	while (itr != v.end())
-	{
-		*itr = 1;
-		std::cout << *itr << std::endl;
-		itr++;
-	}
 
-	for (const auto& e : v) //이렇게 하면 v의 값을 변경할수 없다.
+	for (const auto& e : v)
 	{
 		std::cout << e << std::endl;
 	}
