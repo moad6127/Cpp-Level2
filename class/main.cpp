70e1d0043d8e462711a693d574f8d9e001c79ae5
moad6127 <moad6127@gmail.com>
2022-04-19 17:00:40 +0900

    Feat : Sequence Container
    std::forward_list

--- class/main.cpp
+++ class/main.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include<array>
 #include<vector>
-
+#include<forward_list>
+#include<algorithm>
 
 int main()
 {
 	//std::array<int, 4> container{ 0,1,2,3 };
-	std::vector<int> container{0,1,2,3,4};
+	//std::vector<int> container{0,1,2,3,4};
+	std::forward_list<int> container{ 1,9,11,123,4 };
+
 	//
 	//container.push_back(5);
 	//container.pop_back();
 	//container.erase(container.begin());
-	container.resize(12,4);
-	container.clear();
+
+	container.insert_after(std::find(container.begin(), container.end(), 11),15 );
+	container.erase_after(std::find(container.begin(), container.end(), 11));
+	container.sort(std::greater<int>());
 
 	for (const auto& e : container)
 	{
@@ -24,9 +29,9 @@ int main()
 		std::cout << *itr << " ";
 	}
 	std::cout << "\n";
-	for (int i = 0; i < container.size(); i++)
-	{
-		std::cout << container[i] << " ";
-	}
+	//for (int i = 0; i < container.size(); i++)
+	//{
+	//	std::cout << container[i] << " ";
+	//}
 	std::cout << "\n";
 }
