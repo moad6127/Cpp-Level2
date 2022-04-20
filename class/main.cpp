aa999d1cd11a61ed464de5b694fe3de8ce85b8fc
moad6127 <moad6127@gmail.com>
2022-04-20 15:21:14 +0900

    Feat : std::list

--- class/main.cpp
+++ class/main.cpp
@@ -1,37 +1,19 @@
-#include <iostream>
-#include<array>
-#include<vector>
-#include<forward_list>
+#include<iostream>
+#include<list>
 #include<algorithm>
 
 int main()
 {
-	//std::array<int, 4> container{ 0,1,2,3 };
-	//std::vector<int> container{0,1,2,3,4};
-	std::forward_list<int> container{ 1,9,11,123,4 };
+	std::list<int> container{ 3,2,1 };
 
-	//
-	//container.push_back(5);
-	//container.pop_back();
-	//container.erase(container.begin());
-
-	container.insert_after(std::find(container.begin(), container.end(), 11),15 );
-	container.erase_after(std::find(container.begin(), container.end(), 11));
-	container.sort(std::greater<int>());
+	std::list<int>container2{ 4,5,6 };
+	container.sort();
+	container2.sort();
+	container.merge(container2); //둘다 정렬이 되어있지않으면 에러가 난다.
 
 	for (const auto& e : container)
 	{
 		std::cout << e << " ";
 	}
-	std::cout << "\n";
-	for (auto itr = container.begin(); itr != container.end(); itr++)
-	{
-		std::cout << *itr << " ";
-	}
-	std::cout << "\n";
-	//for (int i = 0; i < container.size(); i++)
-	//{
-	//	std::cout << container[i] << " ";
-	//}
-	std::cout << "\n";
+	std::cout << std::endl;
 }
