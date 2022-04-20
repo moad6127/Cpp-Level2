7e2f931f9d5e5a6d20c5b7dd17463ec77bcb998d
moad6127 <moad6127@gmail.com>
2022-04-20 16:54:34 +0900

    Feat : Associative Container
    std::set
    std::map

--- class/main.cpp
+++ class/main.cpp
@@ -1,39 +1,41 @@
 #include<iostream>
 #include<algorithm>
-#include<stack>
-#include<queue>
+#include<set>
+#include<map>
+using MyPiar = std::pair<std::string, std::string>;
 int main()
 {
-	//std::stack<int> container;
-	//container.push(1);
-	//container.push(2);
-	//container.push(3);
-	//container.pop();
+	//std::set<int> container{8,3,1,6,10,27,4,5,8,8,8};
 
-	//std::queue<int> container;
-	//container.push(1);
-	//container.push(2);
-	//container.push(3);
-	
-	std::priority_queue<int> q;
-	q.push(1);
-	q.push(1);
-	q.push(2);
-	q.push(2);
-	q.push(3);
-	q.push(2);
-	q.push(1);
-	std::cout << q.top() << std::endl;
-	q.pop();
-	std::cout << q.top() << std::endl;
-	//for (const auto& e : container)
-	//{
-	//	std::cout << e << " ";
-	//}
-	/*for (int i = 0; i < container.size(); i++)
+	/*for (const auto& e : container)
 	{
-		std::cout << container[i] << " ";
-	}*/
+		std::cout << e << std::endl;
+	}
+*/
+	std::map<std::string, std::string> container;
+	//"array" : "단순배열"
+	//"vector" : "동적 배열"
+	//"stack"  : "LIFO구조"
+	std::pair<std::string, std::string> element;
+	element.first = "array";
+	element.second = "Simple Static Array";
+	container.insert(element);
 
-	std::cout << std::endl;
+	//constructor
+	container.insert(std::pair<std::string, std::string>("vector", "Dynamic Array"));
+
+	//using alias
+	container.insert(MyPiar("stack", "LIFO"));
+
+	//uniform initialization
+	container.insert({ "queue", "FIFO" });
+
+	
+	for (const auto& e : container)
+	{
+		std::cout << e.first << " : " << e.second << std::endl;
+	}
+
+	std::cout << container.at("array") << std::endl;
+	std::cout << container["array"] << std::endl;
 }
