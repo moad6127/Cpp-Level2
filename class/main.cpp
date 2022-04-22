cd46e7db802aada9dc5fe7bad0491a4f6e25df91
moad6127 <moad6127@gmail.com>
2022-04-22 16:25:44 +0900

    Feat : R-Value Reference
    std::move를 활용하여 우측값 참조

--- class/main.cpp
+++ class/main.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 
-
+class MyClass
+{
+public:
+	int x[100];
+};
+template<typename T>
+void Swap(T& a, T& b)
+{
+	T temp = std::move(a);
+	a =std::move( b) ;
+	b= std::move(temp) ;
+}
 int main()
 {
-	[](int a, int b)
-	{
-		return;
-	};
-
-	[](int a, float b)->bool
-	{
-		return false;
-	};
-
+	int x{ 10 }, y{ 20 };
+	Swap<int>(x, y);
+	std::cout << x << " : " << y << std::endl;
 }
