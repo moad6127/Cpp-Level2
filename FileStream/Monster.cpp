e973553d762bdc40e29c6ce3ba0faf785c48282b
moad6127 <moad6127@gmail.com>
2022-04-28 16:35:05 +0900

    Feat : 파일입출력 바이너리
    데이터 저장

--- FileStream/Monster.cpp
+++ FileStream/Monster.cpp
@@ -4,14 +4,25 @@
 
 std::ifstream& Monster::ReadBinary(std::ifstream& ifs)
 {
-	//ifs.read();
+	int len{};
+	ifs.read(reinterpret_cast<char*>(&len), sizeof(int));
+	mName.resize(len);
+	ifs.read(&mName[0], len);
+	ifs.read(reinterpret_cast<char*>(&mLevel), sizeof(int));
+	ifs.read(reinterpret_cast<char*>(&mHP), sizeof(int));
+	ifs.read(reinterpret_cast<char*>(&mMP), sizeof(int));
+
 	return ifs;
 }
 
 std::ofstream& Monster::WriteBianry(std::ofstream& ofs)
 {
+	//문자열 길이, 문자열, 정수, 정수, 정수
 	int len{static_cast<int>( mName.size()) };
 	ofs.write(reinterpret_cast<char*>(&len), sizeof(int));
-
+	ofs.write(mName.c_str(), len);
+	ofs.write(reinterpret_cast<char*>(&mLevel), sizeof(int));
+	ofs.write(reinterpret_cast<char*>(&mHP), sizeof(int));
+	ofs.write(reinterpret_cast<char*>(&mMP), sizeof(int));
 	return ofs;
 }
--- FileStream/main.cpp
+++ FileStream/main.cpp
@@ -12,21 +12,48 @@ std::ofstream& operator<<(std::ofstream& ofs, Monster& monster)
 	return monster.WriteBianry(ofs);
 }
 
+struct Header
+{
+	int version{ 1 };
+	int monsterCount{};
+};
 
+
+bool SaveToFile(const std::string& fileName, std::vector<Monster> monsters)
+{
+	std::ofstream ofs;
+	ofs.exceptions(std::ofstream::badbit | std::ofstream::failbit);
+	try
+	{
+		ofs.open(fileName, std::ofstream::binary);
+		Header h{ 1,monsters.size() };
+		ofs.write(reinterpret_cast<char*>(&h), sizeof(Header));
+
+		for (auto& e : monsters)
+		{
+			ofs << e;
+		}
+		ofs.close();
+	}
+	catch(std::ofstream::failure e)
+	{
+		std::cout << "저장도중 예외가 발생 : " << e.what() << std::endl;
+		ofs.close();
+		return false;
+	}
+	return true;
+}
+bool LoadFromFile(const std::string& fileName, std::vector<Monster>monsters)
+{
+	return true;
+}
 int main()
 {
 	std::vector<Monster> monsters{
 		{"JELLY",1,1,1},
 		{"WOLF",5,5,5},
-		{"DEMON",5,5,5}
+		{"DEMON",10,10,10}
 	};
+	SaveToFile("Data/SimpleData.dat", monsters);
 
-	//test
-	std::ofstream ofs;
-	ofs.open("Data/SimpleData.dat", std::ofstream::binary);
-	for (auto& e : monsters)
-	{
-		ofs << e;
-	}
-	ofs.close();
 }
