c79a735977ceba6282aada5378609def6d66d2b2
moad6127 <moad6127@gmail.com>
2022-05-02 15:15:11 +0900

    Feat : xml 데이터 입출력

--- SImpleXML/main.cpp
+++ SImpleXML/main.cpp
@@ -29,12 +29,34 @@ void LoadXML(const std::string& filename, std::vector<Sprite>& sprites)
 			std::sregex_iterator current(line.begin(), line.end(), pattern);
 			std::sregex_iterator end;
 
+			int index{};
+			Sprite sprite;
 			while (current != end)
 			{
-				std::cout << ( * current)[1] <<" ";
+				std::string token = (*current)[1];
+
+				switch (index)
+				{
+				case 0:
+					sprite.n = token;
+					break;
+				case 1:
+					sprite.x = std::stoi(token);
+					break;
+				case 2:
+					sprite.y = std::stoi(token);
+					break;
+				case 3:
+					sprite.w = std::stoi(token);
+					break;
+				case 4:
+					sprite.h = std::stoi(token);
+					break;
+				}
 				current++;
+				index++;
 			}
-			std::cout << line << std::endl;
+			sprites.push_back(sprite);
 		}
 	}
 }
